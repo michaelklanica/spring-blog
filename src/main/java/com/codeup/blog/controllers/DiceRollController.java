a917a3daee2baeabd601302b11e21be4b9050693
Michael Klanica <michael.david.klanica@gmail.com>
2020-11-26 06:47:11 -0600

    some commits

---
+++ src/main/java/com/codeup/blog/controllers/DiceRollController.java
@@ -0,0 +1,23 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DiceRollController {
+
+    @GetMapping("/roll-dice/{guess}")
+    @ResponseBody
+    public int diceGame(@PathVariable int guess, Model model) {
+        int randomDiceThrow = (int) (Math.random() * (6 - 1)) + 1;
+        model.addAttribute("roll", randomDiceThrow);
+        return guess;
+
+
+    }
+
+
+}
---
+++ src/main/java/com/codeup/blog/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+
+    }
+
+}
