120231b4922eda1fee5e04a765d80229bc23025f
Michael Klanica <michael.david.klanica@gmail.com>
2020-11-30 18:16:29 -0600

    commits

--- src/main/java/com/codeup/blog/controllers/DiceRollController.java
+++ src/main/java/com/codeup/blog/controllers/DiceRollController.java
@@ -5,25 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Controller
 public class DiceRollController {
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDice(@PathVariable int guess, Model model) {
+        //Find a random number between 1 and 6.
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+        //Compare the guess to the random number.
+        //Store if the number was guessed correctly in a model attribute.
+        model.addAttribute("randomNumber", randomNum);
+        model.addAttribute("myGuess", guess);
+        model.addAttribute("isCorrectGuess", guess == randomNum);
+
+        return "roll-dice";
+    }
 
     @GetMapping("/roll-dice")
-    public String rollDice(){
+    public String displayRollDice() {
         return "roll-dice";
     }
-
-    @GetMapping("/roll-dice/{guess}")
-    public String diceGame(@PathVariable int guess, Model model) {
-        // FIND RANDOM NUMBER BETWEEN 1 and 6
-        int randomDiceThrow = (int) (Math.random() * (7 - 1)) + 1;
-        // COMPARE GUESS TO RANDOM NUMBER
-        // STORE IF NUMBER WAS GUESSED CORRECTLY IN MODEL ATTRIBUTE
-        model.addAttribute("guess", guess);
-        model.addAttribute("roll", randomDiceThrow);
-        model.addAttribute("result", guess == randomDiceThrow);
-        return "roll-dice";
-    }
-
-
 }
